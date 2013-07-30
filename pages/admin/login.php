b97badc50f4625aa44c7dec71ba530a326170975
ozntone <oz.nthr@hotmail.it>
2013-07-30 15:51:00 +0200

    init

---
+++ pages/admin/login.php
@@ -0,0 +1,17 @@
+<?php
+include 'header.php';
+?>
+
+<div class="login">
+    <form method="post" action="autenticate.php">
+        <p>Inserisci l'user</p> <input type="text" name="user"/><br>
+        <p>Inserisci la password</p><input type="password" name="password"/><br>
+        <input type="submit" value="invio"/>
+
+
+
+    </form>
+</div>
+</div>
+</body>
+</html>
