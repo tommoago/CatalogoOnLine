1d0cb7d6eb7f3e9c2e6b9da87354a6d5fe1479e6
ozntone <oz.nthr@hotmail.it>
2013-08-16 18:36:45 +0200

    avanzamento crud prodotti

--- pages/admin/products/prepareInsert.php
+++ pages/admin/products/prepareInsert.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once '../../vendor/twig/twig/lib/Twig/Autoloader.php';
+require_once '../../../vendor/twig/twig/lib/Twig/Autoloader.php';
 Twig_Autoloader::register();
 
-$loader = new Twig_Loader_Filesystem('../../templates');
-$twig = new Twig_Environment($loader, array('cache' => '../../templates/cache',));
-$template = $twig->loadTemplate('insertProductForm.phtml');
+$loader = new Twig_Loader_Filesystem('../../../templates/admin/products');
+$twig = new Twig_Environment($loader, array('cache' => '../../../templates/cache',));
+$template = $twig->loadTemplate('insert.phtml');
 
 $username = "root";
 $password = "root";
