2024a25737e87954120df1c9e2744d624504778e
ozntone <oz.ntone@gmail.com>
2013-08-27 14:33:50 +0200

    image upload e show

--- classes/imgUploader.php
+++ classes/imgUploader.php
@@ -3,7 +3,7 @@ class imgUploader {
 
     var $exts = array(".png", ".gif", ".png", ".jpg", ".jpeg"); //all the extensions that will be allowed to be uploaded 
     var $maxSize = 200000000; //if you set to "0" (no quotes), there will be no limit 
-    var $uploadTarget = "C:\DEV\WP\php\melarossa\images\uploads/"; //make sure you have the '/' at the end 
+    var $uploadTarget = "C:\DEV\WP\php\melarossa\images\uploads/"; //make sure you have the '/' at the end ../images/uploads/
     var $fileName = ""; //this will be automatically set. you do not need to worry about this 
     var $tmpName = ""; //this will be automatically set. you do not need to worry about this 
     var $pathName = "";
@@ -29,8 +29,9 @@ class imgUploader {
     }
 
     public function uploadIt() {
-        $this->pathName = $this->uploadTarget . time() . $this->fileName;
-        return ( move_uploaded_file($this->tmpName, $this->pathName) ? true : false );
+        $finalName = time() . $this->fileName;
+        $this->pathName = '../images/uploads/' . $finalName;
+        return ( move_uploaded_file($this->tmpName, $this->uploadTarget . $finalName) ? true : false );
     }
 
     public function checkSize() {
--- pages/admin/products/delete.php
+++ pages/admin/products/delete.php
@@ -7,9 +7,15 @@ $id = $_GET['id'];
 
 try {
     $DBH = new PDO('mysql:host=localhost;dbname=melarossa', $username, $password);
-    $STH = $DBH->prepare('DELETE FROM products WHERE id = :id');
+    $DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   
+    $STH = $DBH->prepare('DELETE FROM product_images WHERE products_id = :id');
     $data = array('id' => $id);
     $STH->execute($data);
+    
+    $STH = $DBH->prepare('DELETE FROM products WHERE id = :id');
+    $STH->execute($data);
+    header('location:list.php');
 } catch (PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
 }
