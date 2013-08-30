79385d46e0bee13547e76082b3e5e285f80093d6
ozntone <oz.ntone@gmail.com>
2013-08-30 17:11:33 +0200

    committ

--- pages/admin/index.php
+++ pages/admin/index.php
@@ -9,6 +9,6 @@ Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem('../../templates');
 $twig = new Twig_Environment($loader/* , array('cache' => '../../../templates/cache') */);
 $template = $twig->loadTemplate('admin/index.phtml');
-print_r($_SESSION['user']);
+
 $template->display(array('adm' => $_SESSION['user']));
 ?>
