437ef12747babd9eb2909ff50b71fcba25e52647
ozntone <oz.nthr@hotmail.it>
2013-09-03 21:50:14 +0200

    paginazione effettuata su prodotti
    da sistemare e replicare

--- pages/admin/products/list.php
+++ pages/admin/products/list.php
@@ -1,4 +1,5 @@
 <?php
+
 include '../../../classes/dataBase.php';
 require_once '../../../vendor/twig/twig/lib/Twig/Autoloader.php';
 include '../../../classes/Session.php';
@@ -7,36 +8,41 @@ $session = new Session();
 Twig_Autoloader::register();
 
 $loader = new Twig_Loader_Filesystem('../../../templates');
-$twig = new Twig_Environment($loader/*, array('cache' => '../../../templates/cache')*/);
-$template = $twig -> loadTemplate('admin/products/list.phtml');
+$twig = new Twig_Environment($loader/* , array('cache' => '../../../templates/cache') */);
+$template = $twig->loadTemplate('admin/products/list.phtml');
 
 $result = array();
+$offset = $_GET['offset'];
+$limit = 5;
+$numPages = 0;
 
 try {
-	$db = new dataBase();
-	$DBH = $db -> connect();
+    $db = new dataBase();
+    $DBH = $db->connect();
 
-	$stmt = $DBH -> prepare('SELECT COUNT(*) FROM products');
-	$stmt -> execute();
-	$totProd = ($stmt -> fetchAll());
-	$totPages = $totProd[0];
-	echo $totPages[0] / 24;
-	$offset = $_GET{'offset'};
-	$limit = 24;
+    $stmt = $DBH->prepare('SELECT COUNT(*) FROM products');
+    $stmt->execute();
+    $totProd = $stmt->fetch();
+    $count = $totProd[0];
+    $numPages +=  $count/$limit;
+    if($count%$limit != 0){
+        $numPages++;
+    }
+    if($offset != 0 ) $offset *= $limit;
 
-	$stmt = $DBH -> prepare('SELECT * FROM products LIMIT ' . $offset . ', ' . $limit);
-	$stmt -> execute();
-	$result = $stmt -> fetchAll();
+    $stmt = $DBH->prepare('SELECT * FROM products LIMIT ' . $offset . ', ' . $limit);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
 
-	foreach ($result as &$row) {
-		$stmt = $DBH -> prepare('SELECT * FROM categories WHERE id = :id');
-		$stmt -> execute(array('id' => $row['categories_id']));
-		$cat = $stmt -> fetch();
-		$row['category'] = $cat['name'];
-	}
+    foreach ($result as &$row) {
+        $stmt = $DBH->prepare('SELECT * FROM categories WHERE id = :id');
+        $stmt->execute(array('id' => $row['categories_id']));
+        $cat = $stmt->fetch();
+        $row['category'] = $cat['name'];
+    }
 } catch (PDOException $e) {
-	echo 'ERROR: ' . $e -> getMessage();
+    echo 'ERROR: ' . $e->getMessage();
 }
 
-$template -> display(array('prods' => $result, 'totPages' => ($totPages[0] / 24)));
+$template->display(array('prods' => $result, 'totPages' => $numPages));
 ?>
