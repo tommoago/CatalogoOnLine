e75e4d17368f0405f3d2e4fe73644fc19d48777c
ozntone <oz.ntone@gmail.com>
2013-09-10 11:58:12 +0200

    aggiunta modifica per calcolo % prezzi + varie

---
+++ files/db/admins.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM melarossa.administrators
+LIMIT 0, 1000
+
+-- Date: 2013-09-10 11:47
+*/
+INSERT INTO `administrators` (`id`,`name`,`user`,`password`,`role`) VALUES (3,'jack','jack','jack','jack');
+INSERT INTO `administrators` (`id`,`name`,`user`,`password`,`role`) VALUES (4,'operator','op','op','admin');
