2f2c902fb51c1dad7cd4b0860eb599eda5abdaad
ozntone <oz.nthr@hotmail.it>
2013-09-15 16:33:39 +0200

    db

--- files/db/orders.sql
+++
@@ -1,8 +0,0 @@
-/*
--- Query: SELECT * FROM melarossa.orders
-LIMIT 0, 1000
-
--- Date: 2013-09-09 14:25
-*/
-INSERT INTO `orders` (`id`,`data`,`confirmed`,`operator`,`customers_id`) VALUES (1,'2013-03-03 00:00:00',0,'2',4);
-INSERT INTO `orders` (`id`,`data`,`confirmed`,`operator`,`customers_id`) VALUES (2,'2013-04-04 00:00:00',1,'2',4);
