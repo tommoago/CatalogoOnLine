182ba10e241b68f3a8df5c5c01bfc2b4486e2f53
tommoago <tommo.ago@gmail.com>
2014-01-31 11:14:10 +0100

    Revert "form"
    This reverts commit a118dc1719a54bff7dfdb764c3bfc3cab6b67b52.

--- classes/dataBase.php
+++ classes/dataBase.php
@@ -11,19 +11,19 @@ class dataBase {
 
     function connect() {
         
-        /*$conf = array(
+        $conf = array(
             'host' => 'sql.ozntone.com',
             'dbname' => 'ozntonec41438',
             'user' => 'ozntonec41438',
             'pass' => 'oznt50584');
         
-        if($_SERVER['HTTP_HOST'] == 'localhost'){*/
+        if($_SERVER['HTTP_HOST'] == 'localhost'){
             $conf = array(
                 'host' => 'localhost',
                 'dbname' => 'melarossa',
                 'user' => 'root',
                 'pass' => 'root');
-      //  }
+        }
 
         try {
             $DBH = new PDO('mysql:host='.$conf['host'].';dbname='.$conf['dbname'], $conf['user'], $conf['pass']);
--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php
-require_once 'Mobile-Detect-2.7.7/Mobile_Detect.php';
-
-$detect = new Mobile_Detect;
-
-if ($detect -> isMobile()) {
-	header('location:../melarossa.mobile.old');
-} else {
-	header('location:pages/index.php');
-}
+header('location:pages/index.php');
 ?>
